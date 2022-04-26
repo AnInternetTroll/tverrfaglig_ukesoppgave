2df6205601bf7c18b58be6e3f7a1b680360c8faf
AnInternetTroll <lucafulger@gmail.com>
2022-04-26 10:42:32 +0200

    Add logout and error page and documentation kinda

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ passport.use(
 				username
 			);
 			if (!user) {
-				return done(null, false, { message: "Incorrect username." });
+				return done(null, false, { message: "Incorrect username or password." });
 			}
 			pbkdf2(password, user.salt, 1000, 32, "sha256", async (err, key) => {
 				if (err) return done(err);
@@ -93,9 +93,8 @@ passport.use(
 				);
 			});
 		} catch (err) {
-			if (err) {
-				return done(err);
-			}
+			console.error(err);
+			return done(err);
 		}
 	})
 );
@@ -108,7 +107,7 @@ passport.use(
 			callbackURL: process.env.CALLBACK_URL,
 			scope: ["email", "identity"],
 		},
-		async (accessToken, refreshToken, profile, cb) => {
+		async (_accessToken, _refreshToken, profile, cb) => {
 			try {
 				const discordUser = await db.get(
 					"SELECT username FROM users_discord WHERE discord_id = ?",
@@ -155,10 +154,15 @@ passport.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use((req, res, next) => {
+	res.locals.user = req.user;
+	next();
+})
+
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
-app.get("/login", (req, res) =>
+app.get("/login", (_req, res) =>
 	res.render("login", {
 		status: null,
 		discordLink: `https://discord.com/api/oauth2/authorize?client_id=${
@@ -169,17 +173,9 @@ app.get("/login", (req, res) =>
 	})
 );
 
-app.post("/login", restrict, (req, res) => {
+app.post("/login", restrict, (_req, res) => {
 	// If this function gets called, authentication was successful.
-	// "req.user" contains the authenticated user.
-	res.render("login", {
-		status: "Welcome, ${req.user.username}",
-		discordLink: `https://discord.com/api/oauth2/authorize?client_id=${
-			process.env.DISCORD_ID
-		}&redirect_uri=${encodeURIComponent(
-			process.env.CALLBACK_URL
-		)}&response_type=code&scope=${encodeURIComponent("identify email")}`,
-	});
+	res.redirect("/me");
 });
 
 app.get("/discord_login", passport.authenticate("discord"));
@@ -188,15 +184,15 @@ app.get(
 	passport.authenticate("discord", {
 		failureRedirect: "/login",
 	}),
-	(req, res) => {
+	(_req, res) => {
 		res.redirect("/login"); // Successful auth
 	}
 );
 
-app.get("/register", (req, res) => res.render("register", { status: null }));
+app.get("/register", (_req, res) => res.render("register", { status: null }));
 app.post("/register", (req, res) => {
 	const salt = randomBytes(16);
-	pbkdf2(req.body.password, salt, 1000, 32, "sha256", async (err, key) => {
+	pbkdf2(req.body.password, salt, 1000, 32, "sha256", async (_err, key) => {
 		try {
 			await db.run(
 				"INSERT INTO users_local (username, hash, salt) VALUES (?, ?, ?)",
@@ -211,15 +207,27 @@ app.post("/register", (req, res) => {
 			);
 			req.logIn({
 				username: req.body.username,
+			}, (err) => {
+				if (err) {
+					console.error(err);
+					return res.render("error", {message: err.message});
+				}
+				res.redirect("/me");
 			});
-			res.redirect("/login");
 		} catch (err) {
-			res.send(err);
+			console.error(err);
+			res.render("error", { message: err.message });
 		}
 	});
 });
 
-app.get("/users", async (req, res) => {
+app.get("/logout", (req, res) => {
+	req.logOut();
+	// Redirect the user to where they came from
+	res.redirect(req.headers.referer || "/login");
+})
+
+app.get("/users", async (_req, res) => {
 	res.render("users", {
 		users: await db.all("SELECT * FROM users"),
 	});
---
+++ security_details.md
@@ -0,0 +1,41 @@
+# Security
+
+Here at [COMPANY NAME HERE] we take security very seriously.
+One such aspect is how we save user data.
+
+## Users
+
+Users are the backbone of our platform, and as such we need to keep their passwords safe.
+To do so we have implemented a system of hashing and salting passwords.
+
+## Setup
+
+To achieve this level of security we have 4 main frameworks that do the job.
+
+### Express
+
+Express is an HTTP wrapper around node's built in http library.
+It will help us send data and websites to and from the browser.
+It also supports something called "middlewares", which will help us authenticate a user before they are allowed on a website.
+
+### Passport.js
+
+Passport.js is a middleware library for express.
+It will save a cookie in the user's browser, then whenever the user wants to check out a restricted route passport.js will check the cookie for the correct user and if they have permission.
+
+### bcrypt
+
+Bcrypt is a library for hasing passwords.
+It is used to safely save passwords. 
+To find out if a user wrote the same password we take their input, mush it in the same way that we mushed the previous password, and see if the result is the same. 
+If something happens where an attacker finds the hashed password it will be very difficult for them to find out the original password.
+All they can do is try and try again with many passwords until they stumble upon the right one.
+Which is why you still need a strong password.
+
+### SQLite
+
+We use SQLite to store our data. SQLite is a database solution that saves the data to a single file. 
+Unlike PostgreSQL or MySQL/MariaDB or MongoDB there is no setup involved. 
+Just one file. As such it makes the database really portable, while remaining a strong choice for saving data. 
+SQLite uses the SQL syntax, so it will be easy in the future to switch to a more advanced database.
+
